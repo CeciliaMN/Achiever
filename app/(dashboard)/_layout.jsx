709a3d29e8274cd4b3d2d61aca7275cddf492852
Cécilia Myrile N'dri <2393089@csfoy.ca>
2025-10-06 18:01:36 -0400

    Dashboard tab icons

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,6 +1,7 @@
 import { useColorScheme } from "react-native";
 import { Tabs } from "expo-router";
 import { Colors } from "../../constants/Colors";
+import { Ionicons } from '@expo/vector-icons'
 
 export default function DashboardLayout() {
     const colorScheme = useColorScheme();
@@ -9,17 +10,47 @@ export default function DashboardLayout() {
     return (
         <Tabs screenOptions={{
             headerStyle: { backgroundColor: theme.navBackground },
-            headerTintColor: theme.title, 
-            headerShown: false ,
+            headerTintColor: theme.title,
+            headerShown: false,
             tabBarStyle: {
-                backgroundColor: theme.navBackground
+                backgroundColor: theme.navBackground,
+                paddingTop: 3
             },
             tabBarActiveTintColor: theme.iconColorFocused,
             tabBarInactiveTintColor: theme.iconColor
         }}>
-            <Tabs.Screen name="tracking" options={{ title: 'Tracking' }} />
-            <Tabs.Screen name="budgeting" options={{ title: 'Budgeting' }} />
-            <Tabs.Screen name="planning" options={{ title: 'Planning' }} />
+            <Tabs.Screen
+                name="tracking"
+                options={{
+                    title: 'Tracking', 
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            size={30}
+                            name={focused ? "analytics" : "analytics-outline"}
+                            color={focused ? theme.iconColorFocused : theme.iconColor}
+                        />
+                )}} 
+            />
+            <Tabs.Screen name="budgeting" options={{ 
+                title: 'Budgeting', 
+                tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            size={24}
+                            name={focused ? "file-tray-stacked" : "file-tray-stacked-outline"}
+                            color={focused ? theme.iconColorFocused : theme.iconColor}
+                        />
+                )}} 
+            />
+            <Tabs.Screen name="planning" options={{ 
+                title: 'Planning', 
+                tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            size={24}
+                            name={focused ? "calendar-number" : "calendar-number-outline"}
+                            color={focused ? theme.iconColorFocused : theme.iconColor}
+                        />
+                )}} 
+            />
         </Tabs>
     )
 };
